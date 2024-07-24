05716282ddb3d30334ba260ba133db4b88094e45
calvin-c13 <calvinwchau@gmail.com>
2024-07-24 09:15:53 -0400

    completed profile page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,7 +25,7 @@ export default function Header() {
 						<li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
 					</Link>
 					<Link to='/profile'>
-						{currentUser ? (<img className = 'rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />) : <li className='text-slate-700 hover:underline'>Sign In</li>}
+						{currentUser ? (<img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />) : <li className='text-slate-700 hover:underline'>Sign In</li>}
 					</Link>
 				</ul>
 			</div>
