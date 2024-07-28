7fccb7fd41f20bdba447b6c52d0dcd2aa7bb4e6d
calvin-c13 <calvinwchau@gmail.com>
2024-07-28 16:27:44 -0400

    created listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { app } from '../firebase';
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserSuccess, signOutUserStart, deleteUserStart } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -125,6 +126,7 @@ export default function Profile() {
         <input type='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange} id='email' className='border p-3 rounded-lg' />
         <input type='password' placeholder='password' id='password' className='border p-3 rounded-lg' />
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Update'}</button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to = {'/create-listing'}>Create Listing</Link>
       </form>
 
       <div className='flex justify-between mt-5'>
