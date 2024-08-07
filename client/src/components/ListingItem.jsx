843ba75b3a3359766aa126dbf6cc68291566deb4
calvin-c13 <calvinwchau@gmail.com>
2024-08-07 08:21:09 -0400

    added show more listings functionality

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -3,7 +3,7 @@ import { MdLocationOn } from 'react-icons/md';
 
 export default function ListingItem({ listing }) {
     return (
-        <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
+        <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[315px]'>
             <Link to={`/listing/${listing._id}`}>
                 <img src={listing.imageUrls[0]} alt={listing.name} className='h-[320px] sm:h-[220px] w-full object-cover hover: scale-105 transition-scale duration-400' />
 
