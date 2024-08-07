398ffb83ff2386ae10696182c266210b2c50b338
calvin-c13 <calvinwchau@gmail.com>
2024-08-07 17:15:22 -0400

    finished about page and ready to deploy

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -31,12 +31,16 @@ export default function Header() {
 						<span className='text-slate-700'>Estate</span>
 					</h1>
 				</Link>
+
+				
 				<form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
 					<input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
 					<button>
 						<FaSearch className='text-slate-600' />
 					</button>
 				</form>
+
+
 				<ul className='flex gap-4'>
 					<Link to='/'>
 						<li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About CalvinEstate</h1>
+      <p className='mb-4 text-slate-700'>
+        CalvinEstate is the best place to find your next place to live. Our mission is to help our clients achieve their real estate goals by providing expert advice, service, and understanding of the local market.
+      </p>
+    </div>
   )
 }
